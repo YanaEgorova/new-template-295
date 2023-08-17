d462257e65e7eaf038a12c2ce8c1500ae79b8efd
Yana <yanaegorova69.3@gmail.com>
2023-08-17 03:10:28 +0300

    done

--- js/templates/product-item.js
+++ js/templates/product-item.js
@@ -8,22 +8,28 @@ export const item = product => {
     </div>
 
     <div class="product__text-box">
+    <span class="product__price">
+    $${product.price.toFixed(2)}
+  </span>
       <p class="product__name js_product-name">${product.name}</p>
       ${
         product.description[0].text.length > 0
           ? '<p class="text product__desc">' + product.description[0].text.slice(0, 80) + '...' + '</p>'
           : ''
       }
-      <span class="product__price">
-        $${product.price.toFixed(2)}
-      </span>
+ 
     </div>
     </div>
     <div class="product__btn-block">
-      <a href="product-page.html?id=${product.id}" class="btn btn--first" style="margin: ${
+   
+      <a href="product-page.html?id=${product.id}" class="policy-btn btn--first" style="margin: ${
     product.type == 'ring' || product.type == 'clothing' ? 'auto' : ''
-  }">details</a>
-      ${product.type == 'ring' || product.type == 'clothing' ? '' : '<button class="btn js_add-to-cart">buy</button>'}
+  }">details <span></span></a>
+      ${
+        product.type == 'ring' || product.type == 'clothing'
+          ? ''
+          : '<button class="policy-btn js_add-to-cart">buy <span></span></button>'
+      }
     </div>
   </div>
 </li>
